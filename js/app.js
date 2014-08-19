194b6a2116e50f636ce5b7ff5fc1d58712a03abf
nyhunter77 <nyhunter77@yahoo.com>
2014-08-19 08:58:04 -0400

    works as function

--- js/app.js
+++ js/app.js
@@ -1,21 +1,26 @@
 $(document).ready(function() {
 
-var upto=prompt("What number would you like to count up to?");
+//You’ll need to write one named function that takes an integer as argument, and then counts from 1 to the argument value, substituting “fizz”, “buzz”, and “fizzbuzz” accordingly.
 
-for (var i=1; i<=upto; i++) {
-    if (i%3===0 && i%5===0) {
-        console.log ("FizzBuzz")
-        $(".fizzbuzz").append ("<li> FizzBuzz </li>");
-    } else if (i%3===0) {
-        console.log ("Fizz")
-        $(".fizzbuzz").append ("<li> Fizz </li>");
-    } else if (i%5===0) {
-        console.log ("Buzz")
-        $(".fizzbuzz").append ("<li> Buzz </li>");
-    } else {
-        console.log (i);
-        $(".fizzbuzz").append ("<li>" +i+ "</li>");
+var fizzbuzz = function(upto) {
+    for (var i=1; i<=upto; i++) {
+        if (i%3===0 && i%5===0) {
+            console.log ("FizzBuzz")
+            $(".fizzbuzz").append ("<li> FizzBuzz </li>");
+        } else if (i%3===0) {
+            console.log ("Fizz")
+            $(".fizzbuzz").append ("<li> Fizz </li>");
+        } else if (i%5===0) {
+            console.log ("Buzz")
+            $(".fizzbuzz").append ("<li> Buzz </li>");
+        } else {
+            console.log (i);
+            $(".fizzbuzz").append ("<li>" +i+ "</li>");
+        };
     };
-}
+};
+    
+var upto=prompt("What number would you like to count up to?");
+fizzbuzz(upto);
 
 });
